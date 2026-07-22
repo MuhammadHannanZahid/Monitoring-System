6c009096865aef8d833920ddd72378305a09f042
MuhammadHannanZahid <03-134222-062@student.bahria.edu.pk>
2026-07-23 02:39:21 +0500

    Set up MongoDB and connected it to FastAPI

---
+++ Backend/app/core/database.py
@@ -0,0 +1,54 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from pymongo.errors import PyMongoError
+
+from app.core.config import settings
+from app.core.logger import get_logger
+
+logger = get_logger(__name__)
+
+class DatabaseManager:
+    def __init__(self):
+        self._client: AsyncIOMotorClient | None = None
+        self._database: AsyncIOMotorDatabase | None = None
+
+    @property
+    def client(self) -> AsyncIOMotorClient:
+        if self._client is None:
+            raise RuntimeError("MongoDB client has not been initialized.")
+        return self._client
+
+    @property
+    def database(self) -> AsyncIOMotorDatabase:
+        if self._database is None:
+            raise RuntimeError("MongoDB has not been initialized.")
+        return self._database
+
+    async def connect(self) -> None:
+        try:
+            logger.info("Connecting to MongoDB...")
+            self._client = AsyncIOMotorClient(settings.mongo_uri)
+            self._database = self._client[settings.database_name]
+            await self._client.admin.command("ping")
+            logger.info("MongoDB connected successfully.")
+
+        except PyMongoError:
+            logger.exception("Failed to connect to MongoDB.")
+            raise
+
+    async def disconnect(self) -> None:
+        if self._client:
+            logger.info("Closing MongoDB connection.")
+            self._client.close()
+            logger.info("MongoDB connection closed.")
+
+            self._client = None
+            self._database = None
+
+
+    def get_database(self) -> AsyncIOMotorDatabase:
+        return self.database
+
+db_manager = DatabaseManager()
+
+async def get_database() -> AsyncIOMotorDatabase:
+    return db_manager.get_database()
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 from app.core.config import settings
-from app.core.logger import logger
+from app.core.logger import get_logger
+from app.core.database import db_manager
+
+logger = get_logger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Application starting...")
+
+    await db_manager.connect()
+    yield
+    await db_manager.disconnect()
+
+    logger.info("Application stopped.")
 
 app = FastAPI(
     title=settings.app_name,
     version=settings.app_version,
+    lifespan=lifespan,
 )
-
-logger.info("========================================")
-logger.info("%s started", settings.app_name)
-logger.info("Environment : %s", settings.app_env)
-logger.info("Version     : %s", settings.app_version)
-logger.info("========================================")
